15a6a99e6d2af7d93780509546e6f325f94aae6e
dan <marvel94@nhnnext.org>
2015-12-14 13:28:41 +0900

    Initial Commit

---
+++ openGL/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  openGL
+//
+//  Created by 정민 황 on 2015. 12. 14..
+//  Copyright © 2015년 정민 황. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
